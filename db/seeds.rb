a43bc132bdc3ff2b9eb49c43838fdc51e34b8eb3
Ratnam Yadav <ratnam.yadav@icloud.com>
2020-05-26 13:05:04 +0530

    added seed file superadmin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+unless User.exists? role: :super_admin
+  super_admin = User.create(
+      fname: 'Super',
+      lname: 'Admin',
+      email: 'super@admin.com',
+      password: '123456',
+      role: :super_admin,
+      invitation_completed: true)
+  super_admin.skip_confirmation!
+  super_admin.save
+end
