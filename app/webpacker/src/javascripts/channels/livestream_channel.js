e27e41d8c5ed7838b8928f4365753282ec1aac61
Ratnam Yadav <ratnam.yadav@icloud.com>
2020-06-23 05:39:16 +0530

    fixing code for showing livestream through actioncable

--- app/webpacker/src/javascripts/channels/livestream_channel.js
+++ app/webpacker/src/javascripts/channels/livestream_channel.js
@@ -24,7 +24,7 @@ document.addEventListener('turbolinks:load', () => {
           hls.loadSource(videoSrc);
           hls.attachMedia(video);
           hls.on(Hls.Events.MANIFEST_PARSED, function() {
-            video.play();
+            // video.play();
           });
         } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
           video.src = videoSrc;
