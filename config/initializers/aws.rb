35823b06dcd9a96086ff66993b8f630188e75b40
Ratnam Yadav <ratnam.yadav@icloud.com>
2020-06-24 16:21:24 +0530

    small fix

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -4,3 +4,7 @@ Aws.config.update(
     credentials: Aws::Credentials.new(ENV['access_key_id'], ENV['secret_access_key'])
   }
 )
+AWS_SNS_CLIENT = Aws::SNS::Client.new(
+  region: 'us-west-1',
+  credentials: Aws::Credentials.new(ENV['access_key_id'], ENV['secret_access_key'])
+)
