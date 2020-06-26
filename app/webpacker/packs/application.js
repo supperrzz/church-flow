68f66682a087a894d6842597ae021599bcac79bb
Ratnam Yadav <ratnam.yadav@icloud.com>
2020-06-26 17:55:33 +0530

    fixing jwplayer config

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -12,7 +12,7 @@ require('@client-side-validations/client-side-validations');
 import '@fortawesome/fontawesome-free/js/all';
 
 // Added Cocoon
-require("../src/javascripts/cocoon");
+require('../src/javascripts/cocoon');
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
