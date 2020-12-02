df7a46973cdd4a2cc9a036a4efc5c2ed5079eb7b
Ratnam Yadav <ratnam.yadav@icloud.com>
2020-12-02 22:38:15 +0530

    small ui fixes

--- app/services/thumbnail_generate.rb
+++ app/services/thumbnail_generate.rb
@@ -1,7 +1,7 @@
 class ThumbnailGenerate
 
   def create(media_sermon)
-    return if media_sermon.thumbnail.present?
+    return if media_sermon.thumbnail.present? && media_sermon.video.present?
     thumbnail = "media_sermon#{media_sermon.id}.jpeg"
     resp = system "ffmpeg -ss 00:00:05 -i '#{media_sermon.video.url}' -f image2 -vframes 1 '#{thumbnail}'"
     if resp
