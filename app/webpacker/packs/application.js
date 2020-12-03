7518be0a4c675482f5277a4714fdf796b2096e52
Ratnam Yadav <ratnam.yadav@icloud.com>
2020-12-03 13:28:44 +0530

    clipboard plugin added

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -8,6 +8,7 @@ require('turbolinks').start();
 require('@rails/activestorage').start();
 require('../src/javascripts/channels');
 require('bootstrap');
+const clipboard = require('clipboard');
 require('@client-side-validations/client-side-validations');
 import '@fortawesome/fontawesome-free/js/all';
 
@@ -21,6 +22,11 @@ const feather = require('feather-icons');
 document.addEventListener('turbolinks:load', function () {
   feather.replace();
 });
+
+// Clipboard intentiate
+document.addEventListener('turbolinks:load', function () {
+  new clipboard('.clipboard-copy-btn');
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
