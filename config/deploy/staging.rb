ef578d074f9ab9c82190f46fd5b322033f4192a4
Ratnam Yadav <ratnam.yadav@icloud.com>
2021-04-07 19:13:21 +0530

    changed user for staging server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'ec2-18-222-159-252.us-east-2.compute.amazonaws.com', user: 'staging', roles: %w[app db web]
+server 'ec2-18-222-159-252.us-east-2.compute.amazonaws.com', user: 'ubuntu', roles: %w[app db web]
 
 set :branch, 'staging'
 
