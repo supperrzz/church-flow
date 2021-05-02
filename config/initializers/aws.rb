3c3121fae0e3f47a3f7003b182c50b5432be984b
vijayshankar037 <vijay.v@encora.com>
2021-05-02 15:32:47 +0530

    endpoint update aws config

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,7 +1,8 @@
 Aws.config.update(
   {
     region: 'us-east-2',
-    credentials: Aws::Credentials.new(ENV['ACCESS_KEY_ID'], ENV['SECRET_ACCESS_KEY'])
+    credentials: Aws::Credentials.new(ENV['ACCESS_KEY_ID'], ENV['SECRET_ACCESS_KEY']),
+    endpoint:'https://s3.us-east-2.amazonaws.com'
   }
 )
 AWS_SNS_CLIENT = Aws::SNS::Client.new(
