6baac3343e26f36cca6a7735787cdb43ed610719
vijayshankar037 <vijay.v@encora.com>
2021-05-05 23:56:30 +0530

    ffmpeg package and cloudfront path

--- app/jobs/thumbnail_generate_job.rb
+++ app/jobs/thumbnail_generate_job.rb
@@ -6,7 +6,8 @@ class ThumbnailGenerateJob < ApplicationJob
     return if admin_media_sermon.blank? || admin_media_sermon.thumbnail.present? || admin_media_sermon.video.blank?
 
     thumbnail = "media_sermon#{admin_media_sermon.id}.jpeg"
-    resp = system "ffmpeg -ss 00:00:05 -i '#{admin_media_sermon.video.url}' -f image2 -vframes 1 '#{thumbnail}'"
+    
+    resp = system "ffmpeg -ss 00:00:05 -i '#{admin_media_sermon.video&.url(host: ENV['CLOUDFRONT_ASSETS_ENDPOINT'], public: true)}' -f image2 -vframes 1 '#{thumbnail}'"
 
     admin_media_sermon.update(thumbnail: File.open(thumbnail)) if resp
   end
